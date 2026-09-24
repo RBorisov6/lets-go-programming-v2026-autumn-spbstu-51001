4773ca268edc95b9b33a7b9c7ff4196d6ebeb5b5
RBorisov6 <borisovruslan1512@gmail.com>
2026-09-24 15:05:39 +0300

    [TASK-1] change logic to less code

--- borisov.ruslan/task-1/cmd/service/main.go
+++ borisov.ruslan/task-1/cmd/service/main.go
@@ -9,22 +9,17 @@ func main() {
 	var input1, input2, operator string
 	fmt.Scan(&input1, &input2, &operator)
 
-	number1, err1 := strconv.ParseFloat(input1, 64)
-	number2, err2 := strconv.ParseFloat(input2, 64)
-	
-	if err1 != nil {
+	number1, err := strconv.ParseFloat(input1, 64)
+	if err != nil {
 		fmt.Println("Invalid first operand")
 		return
-	} else if err2 != nil {
+	} 
+	
+	number2, err := strconv.ParseFloat(input2, 64)
+	if err != nil {
 		fmt.Println("Invalid second operand")
 		return
-	} else if operator != "+" && operator != "-" && operator != "*" && operator != "/"{
-		fmt.Println("Invalid operation")
-		return
-	} else if operator == "/" && number2 == 0 {
-		fmt.Println("Division by zero")
-		return
-	}
+	} 
 
 	switch operator {
 	case "+":
@@ -34,6 +29,12 @@ func main() {
 	case "*":
 		fmt.Println(number1 * number2)
 	case "/":
+		if number2 == 0 {
+			fmt.Println("Division by zero")
+			return
+		}
 		fmt.Println(number1 / number2)
+	default:
+		fmt.Println("Invalid operation")
 	}
 }
